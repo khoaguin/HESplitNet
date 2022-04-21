c27c2991838117a32c9de73c56fe9056553f1888
khoaguin <dkn.work@protonmail.com>
2022-04-21 10:11:16 +0700

    on debugging the 'encrypted1 and encrypted2 mismatch ' error

--- protocol1/server.py
+++ protocol1/server.py
@@ -108,7 +108,7 @@ class ECGServer256:
             dJda: CKKSTensor = ts.ckks_tensor(context, dJda.tolist(), batch=True)
         else:  # it is CKKSTensor
             try:
-                print(f'Debugging: decrypting W: {self.cache["da2da"].decrypt()}')
+                print(f'Debugging: decrypting W: {self.cache["da2da"].decrypt().tolist()}')
             except:
                 pass
             W_tranpose = self.cache["da2da"].transpose()  # da2da is W
